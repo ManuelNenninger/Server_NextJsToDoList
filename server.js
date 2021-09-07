319caffbe50295fb715fe02975853bfe4df10be0
Manuel <manuel.nenninger@web.de>
2021-09-07 19:11:30 +0200

    back to localhost origin cors and all access

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ var corsOptions = {
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 //Alle Routes sollen bedient werden. Nicht nur eine einzige
-app.use(cors(corsOptions));
+app.use(cors());
 
 
 
